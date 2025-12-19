e8e92d56eac9f4cb60d411bd3f4a9748db03a026
Devanshu Mathur <devanshumathur@Devanshus-MacBook-Air.local>
2025-12-19 11:28:38 +0530

    Final gallery with fixed image paths

--- src/Components/Gallery.jsx
+++ src/Components/Gallery.jsx
@@ -1,22 +1,37 @@
 import React, { useState } from "react";
+import img1 from "../assets/1.jpg"
+import img2 from "../assets/2.jpg"
+import img3 from "../assets/3.jpg"
+import img4 from "../assets/4.jpg"
+import img5 from "../assets/5.jpg"
+import img6 from "../assets/6.jpg"
+import img7 from "../assets/7.jpg"
+import img9 from "../assets/9.jpg"
+import img10 from "../assets/10.jpg"
+import img11 from "../assets/11.jpg"
+import img12 from "../assets/12.jpg"
+import img14 from "../assets/14.jpg"
+
+
 
 export default function GalleryPage() {
   const [activeImg, setActiveImg] = useState(null);
 
   // ⚠️ Images MUST be inside: public/images/
   const images = [
-    "/public/1.jpg",
-    "/public/2.jpg",
-    "/public/3.jpg",
-    "/public/4.jpg",
-    "/public/5.jpg",
-    "/public/6.jpg",
-    "/public/7.jpg",
-    "/public/9.jpg",
-    "/public/10.jpg",
-    "/public/11.jpg",
-    "/public/12.jpg",
-    "/public/14.jpg",
+      img1,
+    img2,
+    img3,
+    img4,
+    img5,
+    img6,
+    img7,
+    img9,
+    img10,
+    img11,
+    img12,
+    img14,
+   
   ];
 
   return (
